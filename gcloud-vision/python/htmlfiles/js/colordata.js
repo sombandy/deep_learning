c804424534e0a56e3ee0450fa3e12abab75d38ac
Somnath Banerjee <sbanerjee1@walmartlabs.com>
2016-05-23 17:17:43 -0700

    Convert to datatables

--- gcloud-vision/python/htmlfiles/js/colordata.js
+++ gcloud-vision/python/htmlfiles/js/colordata.js
@@ -1,29 +1,60 @@
-$.getJSON( "data/colors.json", function( data ) {
-	for(i = 0; i < data.length; i++) {
-		var rgbcolor = rgbColor(data[i].color);
-		var hexcolor = rgbToHex(data[i].color);
+$(document).ready(function() {
+    $('#colortable').DataTable( {
+        "ajax": {
+            "url": "data/colors.json",
+            "dataSrc": function(json) {
+                data = []
+                for(var i = 0; i < json.length; i++) {
+                    var row = {
+                        "score" : json[i].score.toFixed(4),
+                        "pixelFraction" : json[i].pixelFraction.toFixed(4),
+                        "rgb" : rgbColor(json[i].color),
+                        "hexcolor": rgbToHex(json[i].color),
+                        "blank": ""
+                    };
 
-		var tr = $("<tr/>")
-		tr.append("<td>" + data[i].score.toFixed(4) + "</td>");
-		tr.append("<td>" + data[i].pixelFraction .toFixed(4) + "</td>");
-		tr.append("<td>" + rgbcolor + "</td>");
-		tr.append("<td bgcolor='" + hexcolor +"' width='50px'></td>");
-		$('#colordata tbody').append(tr);
-	}
-});
+                    data.push(row);
+                }
+                return data;
+            }
+        },
+
+        "order": [[ 0, "desc" ]],
+
+        "columnDefs": [{
+            "className": "dt-center",
+            "targets": "_all"
+            }],
+
+        "columns": [
+            { "data" : "score" },
+            { "data" : "pixelFraction" },
+            { "data" : "rgb" },
+            { "data" : "blank" },
+        ],
+
+        "createdRow": function( row, data, dataIndex ) {
+            $('td', row).eq(3).css('background-color', data['hexcolor']);
+        },
+
+        "searching": false,
+        "paging":   false,
+        "info":     false
+    } );
+} );
 
 function rgbColor(color) {
-	var r = color.red.toString();
-	var g = color.green.toString();
-	var b = color.blue.toString();
-	return [r, g, b].join(", ")
+    var r = color.red.toString();
+    var g = color.green.toString();
+    var b = color.blue.toString();
+    return [r, g, b].join(", ")
 }
 
 function rgbToHex(color) {
-	var r = color.red;
-	var g = color.green;
-	var b = color.blue;
+    var r = color.red;
+    var g = color.green;
+    var b = color.blue;
 
-	var rgb = b | (g << 8) | (r << 16);
-	return '#' + rgb.toString(16);
+    var rgb = b | (g << 8) | (r << 16);
+    return '#' + rgb.toString(16);
 }
